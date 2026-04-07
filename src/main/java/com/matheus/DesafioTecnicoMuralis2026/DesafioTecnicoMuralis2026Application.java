53e3405a3a654c144ab1a61a1c7537accf1aa090
AbadeMTH <abade.mth@gmail.com>
2026-04-07 09:12:53 -0300

    first commit

---
+++ src/main/java/com/matheus/DesafioTecnicoMuralis2026/DesafioTecnicoMuralis2026Application.java
@@ -0,0 +1,13 @@
+package com.matheus.DesafioTecnicoMuralis2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoMuralis2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoMuralis2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/DesafioTecnicoMuralis2026/DesafioTecnicoMuralis2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.DesafioTecnicoMuralis2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoMuralis2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
