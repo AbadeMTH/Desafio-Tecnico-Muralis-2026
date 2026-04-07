d5ab661d28de89e0d084d2dedd09d6c2078b5b3f
AbadeMTH <abade.mth@gmail.com>
2026-04-07 09:41:11 -0300

    feat: Cria as entidades, se baseando no DER

--- src/main/java/com/matheus/DesafioTecnicoMuralis2026/entity/cliente/ClienteEntity.java
+++ src/main/java/com/matheus/DesafioTecnicoMuralis2026/entity/cliente/ClienteEntity.java
@@ -1,4 +1,34 @@
 package com.matheus.DesafioTecnicoMuralis2026.entity.cliente;
 
+import com.matheus.DesafioTecnicoMuralis2026.entity.contato.ContatoEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_clientes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClienteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private LocalDate data_nascimento;
+    private String endereco;
+
+    @OneToMany
+    private List<ContatoEntity> listaContatos;
 }
--- src/main/java/com/matheus/DesafioTecnicoMuralis2026/entity/contato/ContatoEntity.java
+++ src/main/java/com/matheus/DesafioTecnicoMuralis2026/entity/contato/ContatoEntity.java
@@ -1,4 +1,30 @@
 package com.matheus.DesafioTecnicoMuralis2026.entity.contato;
 
+import com.matheus.DesafioTecnicoMuralis2026.entity.cliente.ClienteEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_contatos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContatoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private int id;
+
+    @Column(nullable = false)
+    private String tipo;
+
+    @Column(nullable = false)
+    private String valor;
+
+    private String observacao;
+
+    @ManyToOne(targetEntity = ClienteEntity.class)
+    private ClienteEntity cliente_id;
 }
